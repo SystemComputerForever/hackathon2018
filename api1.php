2393e30f8b537ccd2a2c25d586ff8f73829cf2dc
renee chan <reneechan122237@gmail.com>
2018-11-03 19:37:12 +0800

    update api1

--- api1.php
+++ api1.php
@@ -1,2 +1,21 @@
 <?php
-echo 'Hello, World!';
+    echo 'Hello, World!';
+
+    // create the Silex application
+    $app = new Application();
+
+    // Create the PDO object for CloudSQL MySQL.
+    $dsn = getenv('MYSQL_DSN');
+    $user = getenv('MYSQL_USER');
+    $password = getenv('MYSQL_PASSWORD');
+    $pdo = new PDO($dsn, $user, $password);
+
+    // Create the database if it doesn't exist
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    
+    $app['pdo'] = $pdo;
+
+    $select = $pdo->prepare('select * from country');
+    $select->execute();
+    var_dump($select);
+?>
